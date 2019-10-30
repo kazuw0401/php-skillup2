d865810585a0f875ad122921967364b28bedebe7
kazuw0401 <kazumasaw0401@gmail.com>
2019-10-30 20:46:20 +0900

    トライアル課題１に着手開始

---
+++ data/trial_1st/form.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="jp">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="./task.php" method="post">
+        <select name="" id="">
+            <option value="president">社長</option>
+            <option value="suzuki">鈴木さん</option>
+            <option value="takahashi">高橋さん</option>
+            <option value="sales_yamada">山田さん</option>
+            <option value="takahashi">中村さん</option>
+            <option value="general_yamada">山田さん</option>
+        </select>
+        <input type="password" name="password" id="">
+    </form>
+</body>
+
+</html>
