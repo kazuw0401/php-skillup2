23e7be103a2e80d0948aafae835c3f2ca2c5ee43
kazuw0401 <kazumasaw0401@gmail.com>
2019-10-31 20:00:18 +0900

    トライアル課題フォーム作成とphp-fpmのDockerfile修正

--- data/trial_1st/form.php
+++ data/trial_1st/form.php
@@ -9,16 +9,23 @@
 </head>
 
 <body>
+	<h1></h1>
     <form action="./task.php" method="post">
-        <select name="" id="">
-            <option value="president">社長</option>
-            <option value="suzuki">鈴木さん</option>
-            <option value="takahashi">高橋さん</option>
-            <option value="sales_yamada">山田さん</option>
-            <option value="takahashi">中村さん</option>
-            <option value="general_yamada">山田さん</option>
-        </select>
-        <input type="password" name="password" id="">
+        <p>
+			お名前：
+            <select name="name" id="">
+				<option value="president">社長</option>
+				<option value="suzuki">鈴木さん（営業チーム）</option>
+				<option value="takahashi">高橋さん（経理チーム）</option>
+				<option value="sales_yamada">山田さん（営業チーム）</option>
+				<option value="takahashi">中村さん（経理チーム）</option>
+				<option value="general_yamada">山田さん（総務チーム）</option>
+			</select>
+		</p>
+		<p>
+			パスワード：<input type="password" name="password" id="">
+		</p>
+        <input type="submit" value="ログイン">
     </form>
 </body>
 
