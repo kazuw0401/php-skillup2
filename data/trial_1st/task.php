9120c4163e4792c4f2800413ed1ef6f25f426df7
kazuw0401 <kazumasaw0401@gmail.com>
2019-11-04 12:42:27 +0900

    trial_1stのコメント追加

--- data/trial_1st/task.php
+++ data/trial_1st/task.php
@@ -1,37 +1,44 @@
 <?php
 
-$postName = htmlspecialchars($_POST['postName'], ENT_QUOTES, 'UTF-8');
-
+$postId = htmlspecialchars($_POST['postId'], ENT_QUOTES, 'UTF-8');
+$postPassword = htmlspecialchars($_POST['password'], ENT_QUOTES, 'UTF-8');
 
 // 社員データが入った配列
 $data = array(
 	
 	0 => array(
+		'id' => 0,
 		'name' => '社長',
 		'password' => '1234',
 	),
 	1 => array(
+		'id' => 1,
 		'name' => '鈴木さん（営業チーム）',
 		'password' => '1111',
 	),
 	2 => array(
+		'id' => 2,
 		'name' => '高橋さん（経理チーム）',
 		'password' => '2222',
 	),
 	3 => array(
+		'id' => 3,
 		'name' => '山田さん（営業チーム）',
 		'password' => '3333',
 	),
 	4 => array(
+		'id' => 4,
 		'name' => '中村さん（経理チーム）',
 		'password' => '4444',
 	),
 	 5 => array(
+		'id' => 5,
 		'name' => '山田さん（総務チーム）',
 		'password' => '5555',
 	),
 );
 
+// タスクの配列を格納
 $tasks = array(
 	0 => array(''),
 	1 => array('A社訪問', 'B社資料送付'),
@@ -41,10 +48,22 @@ $tasks = array(
 	5 => array('求人開始', '退職予定者の処理作業'),
 );
 
-if($data['name'] == $postName) {
-
+// 入力された名前とパスワードが一致していた場合、タスクを表示
+if($data[1]['id'] == $postId && $data[1]['password'] == $postPassword) { ?>
+	<p><?php echo $data[1]['name']; ?>さんのタスク一覧</p>
+	<?php
+	foreach($tasks as $task => $value) { 
+		echo $value[1].'<br>';
+	}
+} else {
+		echo 'パスワードが違います。';
+	// echo $tasks[1].'<br>';
 }
 
+// foreach($tasks as $task => $value) {
+// 	echo $value;
+// }
 
-echo $data[1]['name'].'<br>';
-echo $tasks[1][1].'<br>';
+
+// echo $data[1]['name'].'<br>';
+// echo $tasks[1][1].'<br>';
